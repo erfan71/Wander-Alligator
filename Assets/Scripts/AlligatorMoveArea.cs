eb2216d04beb5d19f77b3ae38cd628dd1ae07c2d
erfan <erfantavakoly@comp.iust.ac.ir>
2018-10-24 14:27:12 +0330

    Basic Editable Move Area with Gizmoz and some function to test Alligator movement on it

---
+++ Assets/Scripts/AlligatorMoveArea.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AlligatorMoveArea : MonoBehaviour
+{
+    [SerializeField] 
+    private List<Transform> points;
+    void Start()
+    {
+
+        points = new List<Transform>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            points.Add(transform.GetChild(i));
+
+        }
+    }
+    private void Update()
+    {
+#if UNITY_EDITOR
+        points = new List<Transform>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            points.Add(transform.GetChild(i));
+
+        }
+#endif
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        
+        for ( int i=0; i < points.Count; i++)
+        {
+            if (i != points.Count - 1)
+            {
+                Gizmos.DrawLine(points[i].position, points[i + 1].position);
+            }
+            else
+            {
+                Gizmos.DrawLine(points[i].position, points[0].position);
+            }
+        }
+    }
+    public List<Transform> GetPoints()
+    {
+        return points;
+    }
+
+}
---
+++ Assets/Scripts/AlligatorMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlligatorMovement : MonoBehaviour {
+
+    // Use this for initialization
+    public AlligatorMoveArea moveArea;
+    List<Transform> points;
+
+    void Start () {
+        MoveAlligatorOntheEdgesPoints();
+
+    }
+    void MoveAlligatorOntheEdgesPoints()
+    {
+        points = moveArea.GetPoints();
+        if (points.Count >= 2)
+        {
+            MoveBetweenTwoPoint(0);
+
+        }
+    }
+    void MoveBetweenTwoPoint(int index)
+    {
+        if ( index!= points.Count-1)
+        StartCoroutine(MoveBetweenTwoPointRoutine(points[index].position, points[index + 1].position,2, () => MoveBetweenTwoPoint(index + 1)));
+        else
+            StartCoroutine(MoveBetweenTwoPointRoutine(points[index].position, points[0].position, 0.5f, () => MoveBetweenTwoPoint(0)));
+
+    }
+    IEnumerator MoveBetweenTwoPointRoutine(Vector3 pointFrom, Vector3 pointTo, float speed,System.Action finishedCallback)
+    {
+        float i = 0.0f;
+        float rate = speed;
+        while (i < 1)
+        {
+            i += rate * Time.deltaTime;
+            transform.position = Vector3.Lerp(pointFrom, pointTo,i);
+            yield return null;
+        }
+        transform.position = pointTo;
+        finishedCallback?.Invoke();
+    }
+	
+	
+}
